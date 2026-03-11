ea8b5b8ac2e2c949516a5376a146a25f9b4267d0
Visar122 <shabaniwiisar@gmail.com>
2026-03-11 05:35:54 +0100

    Home side opd

--- AutoFlow-Forntend/src/app/home/home.ts
+++ AutoFlow-Forntend/src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SHARED } from '../shared';
+import { FadeIn } from '../fade-in';
 
 @Component({
   selector: 'app-home',
-  imports: [SHARED],
+  imports: [SHARED, FadeIn],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
